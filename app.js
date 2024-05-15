47bdf8ad63029126def613769e6b55383f0c7de3
Dean <deaneffi1414@gmail.com>
2024-05-15 14:34:22 +0300

    add register form and styles, add roboto font

--- app.js
+++ app.js
@@ -23,4 +23,13 @@ app.get("/", function (req, res) {
   res.render("index");
 });
 
+app.get("/register", function (req, res) {
+  res.render("register");
+});
+
+app.post("/register", function (req, res) {
+  console.log(req.body);
+  res.render("register");
+});
+
 app.listen(3000);
