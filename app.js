2f704c70d8acf98cf9a45daff3534cae79ca71ac
Dean <deaneffi1414@gmail.com>
2024-05-15 12:59:25 +0300

    get session working, add basic pug layout

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const session = require("express-session");
 const app = express();
 
 require("dotenv").config();
@@ -9,6 +9,15 @@ app.set("view engine", "pug");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static("public"));
+app.use(
+  session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 1000 * 60 },
+  })
+);
 
 app.get("/", function (req, res) {
   res.render("index");
